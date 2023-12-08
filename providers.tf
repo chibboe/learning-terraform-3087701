c516425d04c96f6c03b6e68ecea4de380e69ea7c
chibboe <149611773+chibboe@users.noreply.github.com>
2023-12-08 12:03:26 +0000

    yes

--- providers.tf
+++ providers.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "Chibboe_module" {
-  source = "./modules/Chibboe_module"
+  source = "modules/Chibboe_module"
 
   # You can pass any module-specific variables here if needed
   # For example, title = "Perceptions"
