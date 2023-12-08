f2f318466aa348ea5ca1d5ea01d09e0b91cae383
chibboe <149611773+chibboe@users.noreply.github.com>
2023-12-08 11:56:45 +0000

    try

--- providers.tf
+++ providers.tf
@@ -1,12 +1,22 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
+#terraform {
+  #required_providers {
+    #aws = {
+     # source  = "hashicorp/aws"
     }
   }
 }
 
-provider "aws" {
-  region  = "us-east-2"
+#provider "aws" {
+  #region  = "us-east-2"
 }
 
+provider "aws" {
+  region = "us-east-1" # Choose your desired AWS region
+}
+
+module "Chibboe_module" {
+  source = "./modules/Chibboe_module"
+
+  # You can pass any module-specific variables here if needed
+  # For example, title = "Perceptions"
+}
