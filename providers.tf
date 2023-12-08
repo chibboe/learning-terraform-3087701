1b286341262293808408eabebaa6a6e26020e4ed
chibboe <149611773+chibboe@users.noreply.github.com>
2023-12-08 12:04:47 +0000

    yes

--- providers.tf
+++ providers.tf
@@ -1,9 +1,6 @@
 provider "aws" {
-  region = "us-east-1" # Choose your desired AWS region
+  region = "us-east-2" # Choose your desired AWS region
 }
-
-module "Chibboe_module" {
-  source = "modules/Chibboe_module"
 
   # You can pass any module-specific variables here if needed
   # For example, title = "Perceptions"
